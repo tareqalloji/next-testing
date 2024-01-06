769a0645027f5117c466420c4d87ab0fc37717cd
tareqalloji <tarq.alloji@gmail.com>
2024-01-06 14:45:21 +0200

    testing.

--- src/Services/APIs/Auth.tsx
+++ src/Services/APIs/Auth.tsx
@@ -1,11 +1,11 @@
 import axios from "axios";
-const BASEURL = `${process.env.NEXT_PUBLIC_BASEURL}`;
+const BASEURL = `https://event-reg.app/flutter_test/api`;
 export default class Api {
   Login(data: any) {
-    return axios.post(`https://event-reg.app/flutter_test/api/login`, data, {});
+    return axios.post(`${BASEURL}/login`, data, {});
   }
   Register(data: any) {
-    return axios.post(`https://event-reg.app/flutter_test/api/register`, data, {});
+    return axios.post(`${BASEURL}/register`, data, {});
   }
 }
 
